414cdb746102ff6254c9419f9f5e154c2a4d3805
huozhenwei <zhenwei.huo@bjrxtd.com>
2022-03-01 18:08:41 +0800

    自定义 hooks

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,8 +2,22 @@
 <template>
     <h1>这是关于页面</h1>
     <h1 @click="toggle">click</h1>
+    <div v-if="todos.loading">loading todo!</div>
+    <div v-else>{{ todos.result.title }}</div>
+    <div v-if="posts.loading">loading posts!</div>
+    <div v-else>{{ posts.result.body }}</div>
 </template>
 <script setup>
+// interface PostProps {
+//     userId: number;
+//     id: number;
+//     title: string;
+//     body: string;
+// }
 import { useFullscreen } from '@vueuse/core'
+import useURLLoader from '../utils/useURLLoader';
 const { isFullscreen, enter, exit, toggle } = useFullscreen()
+const todos = useURLLoader('https://jsonplaceholder.typicode.com/todos/1')
+// const posts = useURLLoader < PostProps > ('https://jsonplaceholder.typicode.com/posts/1')
+const posts = useURLLoader('https://jsonplaceholder.typicode.com/posts/1')
 </script>
