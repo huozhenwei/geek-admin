9c4383fd4c08ed2a4a456304fb486309ac08b218
huozhenwei <718289183@qq.com>
2022-03-17 20:14:25 +0800

    安装组件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
+
 import App from './App.vue'
 import store from './store/index'
 
@@ -7,4 +11,5 @@ import router from './router/index'
 createApp(App)
 .use(router)
 .use(store)
+.use(Element3)
 .mount('#app')
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -8,19 +8,24 @@
   <div v-else>{{ posts.result.body }}</div>
 
   <h1>你的评分是 {{ score }}</h1>
-  <Rate v-model="score" >课程评分</Rate>
+  <Rate v-model="score">课程评分</Rate>
+
+  <Heading :level="3">hello geekbang</Heading>
 </template>
-<script setup>
+<script setup lang="ts">
 // interface PostProps {
 //     userId: number;
 //     id: number;
 //     title: string;
 //     body: string;
 // }
-import { ref } from 'vue'
+// import { ref } from 'vue'
+import { computed, reactive, ref } from '@vue/runtime-core'
 import { useFullscreen } from '@vueuse/core'
 import useURLLoader from '../utils/useURLLoader'
 import Rate from '../components/Rate.vue'
+
+import Heading from '../components/Heading.jsx'
 
 const { isFullscreen, enter, exit, toggle } = useFullscreen()
 const todos = useURLLoader('https://jsonplaceholder.typicode.com/todos/1')
@@ -31,4 +36,19 @@ let score = ref(3.5)
 function update(num) {
   score.value = num
 }
+
+interface 极客时间课程 {
+  name: string
+  price: number
+}
+
+const msg = ref('') //  根据输入参数推导字符串类型
+const msg1 = ref<string>('') //  可以通过范型显示约束
+const obj = reactive({})
+const course = reactive<极客时间课程>({ name: '玩转vue3全家桶', price: 89 })
+
+const msg2 = computed(() => '') // 默认参数推导
+const course2 = computed<极客时间课程>(() => {
+  return { name: '玩转vue3全家桶', price: 89 }
+})
 </script>
