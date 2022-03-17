68a5709c9a867d7e77688a122b80b7352f7eeb19
huozhenwei <718289183@qq.com>
2022-03-17 14:42:52 +0800

    min router

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,33 @@
 
-import {
-    createRouter,
-    createWebHashHistory,
-  } from 'vue-router'
-  import Home from '../pages/home.vue'
-  import About from '../pages/about.vue'
-  import Trans from '../pages/trans.vue'
-  
-  const routes = [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
-      path: '/trans',
-      name: 'Trans',
-      component: Trans
-    }
-  ]
-  
-  const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-  })
-  
-  export default router
-  
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// import { createRouter, createWebHashHistory } from './grouter/index'
+
+import Home from '../pages/home.vue'
+import About from '../pages/about.vue'
+import Trans from '../pages/trans.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/trans',
+    name: 'Trans',
+    component: Trans
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
